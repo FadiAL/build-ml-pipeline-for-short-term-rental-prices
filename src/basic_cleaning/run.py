41fadfee7bbd9931358acf78dd2c5260ef23d3f1
Fadi Alladkani <fadialladkani@gmail.com>
2023-10-28 18:34:07 -0600

    Added some descriptions, added latitude and longitude cleanup

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,10 @@ def go(args):
     logger.info("Converting 'last_review' to datetime")
     df["last_review"] = pd.to_datetime(df["last_review"])
 
+    logger.info("Constraining latitude and longitude to wtihin acceptable range")
+    idx = df["longitude"].between(-74.25, -73.50) & df["latitude"].bewteen(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving cleaned data to csv with pandas")
     df.to_csv("clean_sample.csv", index=False)
 
