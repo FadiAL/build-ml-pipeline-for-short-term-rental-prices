056f42ffaf5ee45bca9a3bd36e6fb60e76d92667
Fadi Alladkani <fadialladkani@gmail.com>
2023-10-30 17:54:42 -0600

    Formatted with autopep8

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -40,7 +40,8 @@ def go(args):
     logger.info("Loading artifact with pandas")
     df = pd.read_csv(local_path)
 
-    logger.info(f"Filtering price to range ({args.min_price}, {args.max_price})")
+    logger.info(
+        f"Filtering price to range ({args.min_price}, {args.max_price})")
     idx = df["price"].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
@@ -48,7 +49,11 @@ def go(args):
     df["last_review"] = pd.to_datetime(df["last_review"])
 
     logger.info("Constraining latitude and longitude to wtihin acceptable range")
-    idx = df["longitude"].between(args.min_longitude, args.max_longitude) & df["latitude"].between(args.min_latitude, args.max_latitude)
+    idx = df["longitude"].between(
+        args.min_longitude,
+        args.max_longitude) & df["latitude"].between(
+        args.min_latitude,
+        args.max_latitude)
     df = df[idx].copy()
 
     logger.info("Saving cleaned data to csv with pandas")
@@ -56,9 +61,9 @@ def go(args):
 
     logger.info("Logging artifact to W&B")
     artifact = wandb.Artifact(
-      args.output_artifact,
-      type=args.output_type,
-      description=args.output_description
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
     )
     artifact.add_file("clean_sample.csv")
     run.log_artifact(artifact)
@@ -68,44 +73,43 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
-
     parser.add_argument(
-        "--input_artifact", 
+        "--input_artifact",
         type=str,
         help="Input artifact to clean",
         required=True
     )
 
     parser.add_argument(
-        "--output_artifact", 
+        "--output_artifact",
         type=str,
         help="Output artifact name to save to W&B",
         required=True
     )
 
     parser.add_argument(
-        "--output_type", 
+        "--output_type",
         type=str,
         help="Type of output artifact",
         required=True
     )
 
     parser.add_argument(
-        "--output_description", 
+        "--output_description",
         type=str,
         help="Description of output artifact",
         required=True
     )
 
     parser.add_argument(
-        "--min_price", 
+        "--min_price",
         type=float,
         help="Minimum price to restrict",
         required=True
     )
 
     parser.add_argument(
-        "--max_price", 
+        "--max_price",
         type=float,
         help="Maximum price to restrict",
         required=True
